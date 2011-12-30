2bc5e6d969d1427885fa042946f59576be858454
Sam Rudge <samarudge@gmail.com>
2011-12-30 18:28:23 +0000

    Initial test commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+ 
+from setuptools import setup, find_packages
+import sys, os
+ 
+version = '0.7'
+ 
+setup(name='squeezeit',
+	version=version,
+	author='Sam Rudge',
+	author_email='sam@rmg.io',
+	packages=['squeezeit'],
+	include_package_data=True,
+	zip_safe=False,
+	entry_points="""
+	[console_scripts]
+	squeezeit = squeezeit.cli:start
+	"""
+	)
---
+++ squeezeit/cli.py
@@ -0,0 +1,25 @@
+"""
+Squeezeit can be run from the command line using the 'squeezeit' command
+"""
+
+import squeezeit
+
+def start():
+	"""
+	Start processing from the command line
+	
+	Loads the path to the config (from the argument variables) and runs the minification process
+	
+	run with
+	squeezeit [relative path to bundle config file]
+	"""
+	
+	#Confirm the command line arguments exist
+	if len(squeezeit.sys.argv) <= 1:
+		squeezeit.logging.critical("Pass the location of the bundle file. See readme")
+		squeezeit.sys.exit(1)
+	
+	bundleconfig = squeezeit.sys.argv[1]
+	
+	#Start the processing (Yes that's about it for this file)
+	squeezeit.compress(bundleconfig)
